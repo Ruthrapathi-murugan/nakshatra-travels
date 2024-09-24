928d09901fe5fa0ea16d5f8e63e6f2cf33437ba7
Ruthrapathi <ruthrapathimurugan@outlook.com>
2024-09-24 14:45:53 +0530

    new

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div className="container my-5">
-      <h2>About Nakshatra Travels</h2>
+      <h2>About Nakshatra Travels in Palani</h2>
       <p>Nakshatra Travels offers a wide range of travel services, from booking flights to arranging luxurious hotel stays and memorable tours. Our mission is to provide top-notch travel experiences that cater to all your needs.</p>
     </div>
   );
